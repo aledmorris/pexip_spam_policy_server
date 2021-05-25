bb1079274e2aecef56da0894944f1f3163cefd6f
aledmorris <aled.morris@pexip.com>
2021-05-25 20:20:00 +1000

    Added most wanted test

--- cloud_func.py
+++ cloud_func.py
@@ -12,36 +12,58 @@ def spam_policy(request):
     """
     from flask import abort, jsonify
 
-    suspect_vendor = ["cisco", "friendly-scanner", "sipcli", "sipvicious", "sip-scan", "sipsak", "sundayddr", "iWar", "CSipSimple", "SIVuS", "Gulp", "sipv",  "smap", "friendly-request", "VaxIPUserAgent", "VaxSIPUserAgent", "siparmyknife", "Test Agent", "PortSIP VoIP SDK 11.2", "ABTO Video SIP SDK", "Asterisk PBX", "PortSIP VoIP SDK", "eyeBeam release 3015c stamp 27107", "ciscovoipswichpbx"]
+    # grab values from the request to check
+    call_direction = request.args['call_direction']
+    user_agent = request.args['vendor']
+    ua_lower = user_agent.lower()
+    
+    # policy responses
+    success_continue =  {
+                            "status" : "success",
+                            "action" : "continue"
+                        }
 
-    if request.method == 'GET':
-        
-        # grab values to check
-        call_direction = request.args['call_direction']
-        user_agent = request.args['vendor']
-
-        if call_direction == 'dial_in':
-            if user_agent in suspect_vendor:
-                # reject the call - UA known to be bad            
-                policy_response =                 {
+    fail_reject =   {
                         "status" : "success",
                         "action" : "reject",
                         "reason" : "Known bad user agent, " + user_agent + ", Bang Bang!"
-                    }           
-                
-            else:
-                # permit the call - not a known bad UA
-                policy_response =                 {
-                        "status" : "success",
-                        "action" : "continue"
                     }
+
+    # the list of known bad user agents
+    suspect_vendor = ["cisco", "friendly-scanner", "sipcli", "sipvicious", "sip-scan", "sipsak", "sundayddr", "iWar", "CSipSimple", "SIVuS", "Gulp", "sipv",  "smap", "friendly-request", "VaxIPUserAgent", "VaxSIPUserAgent", "siparmyknife", "Test Agent", "PortSIP VoIP SDK 11.2", "ABTO Video SIP SDK", "Asterisk PBX", "PortSIP VoIP SDK", "eyeBeam release 3015c stamp 27107", "ciscovoipswichpbx", "ciscovoipswichpbx123" ]
+    
+    # prefix of the the most popular known bad user agents
+    most_wanted = [ "portsip", "eyebeam", "ciscovoip" ]
+
+    if request.method == 'GET':
+        
+        match = False
+
+        if call_direction == 'dial_in':
+            
+            for ua in most_wanted:
+            
+                if ua_lower.startswith(ua) == True:
+                    # reject the call - UA known to be bad 
+                    policy_response = fail_reject
+                    #log the rejection
+                    print("Blocked User Agent: " + user_agent)
+                    match=True
+
+            if match==False:
+                if user_agent in suspect_vendor:
+                    # reject the call - UA known to be bad            
+                    policy_response = fail_reject
+                    #log the rejection
+                    print("Blocked User Agent: " + user_agent)    
+                
+                else:
+                    # permit the call - not a known bad UA
+                    policy_response = success_continue
                 
         else:
             # permit the call - call is not inbound
-            policy_response =                 {
-                        "status" : "success",
-                        "action" : "continue"
-                    }
+            policy_response = success_continue
         
         # return the api response
         return jsonify(policy_response)
